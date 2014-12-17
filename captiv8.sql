a829c61c5fb8e49a456cfa79ae5482883da6b995
nolvorite <hns_marcon@hotmail.com>
2014-12-17 10:24:27 -0600

    almost forgot the DB~~

--- captiv8.sql
+++ captiv8.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 16, 2014 at 10:52 PM
+-- Generation Time: Dec 17, 2014 at 04:24 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.16
 
@@ -231,8 +231,17 @@ CREATE TABLE IF NOT EXISTS `sg_permissions` (
   `access_type` varchar(30) NOT NULL,
   `towhom` varchar(30) NOT NULL,
   `date_g` date NOT NULL,
-  `granted_by` varchar(35) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `granted_by` varchar(35) NOT NULL,
+  `sgp_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`sgp_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `sg_permissions`
+--
+
+INSERT INTO `sg_permissions` (`access_type`, `towhom`, `date_g`, `granted_by`, `sgp_id`) VALUES
+('friend snowglobe', 'nolvorite', '2014-12-16', 'nerd', 1);
 
 -- --------------------------------------------------------
 
@@ -260,10 +269,7 @@ INSERT INTO `sg_settings` (`id`, `a1_check`, `a2_check`, `a3_check`, `a4_check`,
 ('user_27', 'all', 'all', 'all', 'self', 'self', 2),
 ('user_31', 'all', 'all', 'all', 'self', 'self', 3),
 ('user_32', 'all', 'all', 'all', 'self', 'self', 4),
-('', 'all', 'all', 'all', 'self', 'self', 5),
-('', 'all', 'all', 'all', 'self', 'self', 6),
-('', 'all', 'all', 'all', 'self', 'self', 7),
-('', 'all', 'all', 'all', 'self', 'self', 8);
+('user_36', 'all', 'all', 'all', 'self', 'self', 5);
 
 -- --------------------------------------------------------
 
@@ -289,7 +295,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `login_att_last` datetime DEFAULT CURRENT_TIMESTAMP,
   `last_active_at` datetime DEFAULT '0000-00-00 00:00:00',
   `last_activity` varchar(30) DEFAULT NULL,
-  `friends` mediumtext,
   PRIMARY KEY (`userid`),
   UNIQUE KEY `root_admin_status` (`root_admin_status`),
   UNIQUE KEY `username` (`username`)
@@ -299,12 +304,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `password`, `isvalidated`, `email`, `userid`, `joindate`, `fullname`, `admin_notifs`, `root_admin_status`, `month2`, `day2`, `year2`, `salt`, `login_attempts`, `login_att_last`, `last_active_at`, `last_activity`, `friends`) VALUES
-('nolvorite', 'd83ab46b4325d8bcd1102188c38d8a098b23a168', 1, 'hns_marcon@hotmail.com', 1, '2014-11-14 23:52:34', 'Hans Marcon', 'on', '1', 4, 9, 1996, '6c478c525f517a7ab838', '0', '2014-12-15 13:31:51', '2014-12-16 13:16:44', NULL, NULL),
-('test_user4aaaasd', 'c08c7f680792684058eafc92e8e1e85e07617eff', NULL, 'asdasdas@asda.asdas', 27, '2014-11-13 21:16:30', 'asdas asdas', 'on', NULL, 12, 16, 1994, 'aad763cbecdacccb1f63', '0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, NULL),
-('test_9', '6e1d3ee1ec90c3617dc4aff58fe39bba75a8b68e', NULL, 'laskdla@alsdkalsd.asdjas', 31, '2014-11-14 23:21:41', 'asdajsdjaks asdjaks', 'on', NULL, 4, 19, 1996, '3aab2407326f6ada6661', '0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, NULL),
-('asdasdas', 'ef659ade9df94a0bc3e792ec9e2f81095d375a48', NULL, 'hns_marcon@hotmail.coma', 32, '2014-11-13 21:16:30', 'asdasd asd asdas', 'on', NULL, 11, 15, 1999, 'e64ec838d0dba46ea10a', '0', '2014-12-02 00:06:50', '0000-00-00 00:00:00', NULL, NULL),
-('nerd', '0989807af1ccfc712499519ebcb0e742b0597918', NULL, 'nerd@nerd.nerd', 36, '2014-12-14 22:10:24', 'John Hammercock', 'on', NULL, 6, 5, 1976, '9f5106ab7ebfe0e42b9a', '0', '2014-12-14 22:10:24', '2014-12-14 23:53:08', NULL, NULL);
+INSERT INTO `users` (`username`, `password`, `isvalidated`, `email`, `userid`, `joindate`, `fullname`, `admin_notifs`, `root_admin_status`, `month2`, `day2`, `year2`, `salt`, `login_attempts`, `login_att_last`, `last_active_at`, `last_activity`) VALUES
+('nolvorite', 'd83ab46b4325d8bcd1102188c38d8a098b23a168', 1, 'hns_marcon@hotmail.com', 1, '2014-11-14 23:52:34', 'Hans Marcon', 'on', '1', 4, 9, 1996, '6c478c525f517a7ab838', '0', '2014-12-15 13:31:51', '2014-12-17 01:15:20', NULL),
+('test_user4aaaasd', 'c08c7f680792684058eafc92e8e1e85e07617eff', NULL, 'asdasdas@asda.asdas', 27, '2014-11-13 21:16:30', 'asdas asdas', 'on', NULL, 12, 16, 1994, 'aad763cbecdacccb1f63', '0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL),
+('test_9', '6e1d3ee1ec90c3617dc4aff58fe39bba75a8b68e', NULL, 'laskdla@alsdkalsd.asdjas', 31, '2014-11-14 23:21:41', 'asdajsdjaks asdjaks', 'on', NULL, 4, 19, 1996, '3aab2407326f6ada6661', '0', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL),
+('asdasdas', 'ef659ade9df94a0bc3e792ec9e2f81095d375a48', NULL, 'hns_marcon@hotmail.coma', 32, '2014-11-13 21:16:30', 'asdasd asd asdas', 'on', NULL, 11, 15, 1999, 'e64ec838d0dba46ea10a', '0', '2014-12-02 00:06:50', '0000-00-00 00:00:00', NULL),
+('nerd', '0989807af1ccfc712499519ebcb0e742b0597918', NULL, 'nerd@nerd.nerd', 36, '2014-12-14 22:10:24', 'John Hammercock', 'on', NULL, 6, 5, 1976, '9f5106ab7ebfe0e42b9a', '0', '2014-12-14 22:10:24', '2014-12-14 23:53:08', NULL);
 
 -- --------------------------------------------------------
 
