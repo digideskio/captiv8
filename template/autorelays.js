5f297f7314d10b3e075d4337e29a1ddfff99071e
nolvorite <hns_marcon@hotmail.com>
2014-12-18 13:38:42 -0600

    Tiny fixes.

--- template/autorelays.js
+++ template/autorelays.js
@@ -1,4 +1,7 @@
 //load notifications, online status, etc
+//hmmm
+
+
 
 setInterval(function(){
 $.ajax({
@@ -9,16 +12,30 @@ if(data.result == "new"){
 if($("#notifs_bar .spacer a").next().is(":not('.note')")){
 $("#notifs_bar .spacer a").after(" <span class='note'>("+data.notifs.length+" new)</span>");  } 
 $.each(data.notifs,function(i,v){ 
-if($("#notifications .notifs").length != data.notifs.length){ 
-$("#notifications .spacer").prepend("<div class='notifs'>"+data.notifs[i]['content']+"</div>");
-$("#notifications .spacer .notifs:first-child").wrapInner("<a href='"+ data.notifs[i]['url'] +"'></a>");
+if($(".harrr").length != data.notifs.length){ 
+$("#notifications .spacer").prepend("<span alt='"+ data.notifs[i]['url'] +"' class='harrr'>"+data.notifs[i]['content']+"</span>");
+//$("<a href='"+ data.notifs[i]['url'] +"'><div class='notifs'>Some arbitrary content "+data.notifs[i]['content']+"</div></a>").prependTo("#notifications .spacer");
 }
-});    
+});
+
+
+    
 $zin = $("#notifications .spacer").html();
 if($("#clear").length == 0){
 $("#notifications .spacer").html( $zin + "<a href='clear' class='prompt' id='clear'>Clear all notifications</a>");      
-}                  
+}  
+
+                
 }else{} 
 },dataType: "json"        
 });
+  $('.harrr').each(function(){
+$zen = $(this).attr('alt');    
+if($(this).parent().is(":not('.notifs')")){
+$(this).wrap("<a href='"+ $zen +"'><div class='notifs'></div></a>");    //that was painful
+     }
+});
+
 },2500);
+          
+            
